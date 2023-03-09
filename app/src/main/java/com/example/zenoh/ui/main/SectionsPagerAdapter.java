a63b3fba5068994f8ac15ecaee13d8d7446606bf
Darius Maitia <darius@zettascale.tech>
2023-03-09 10:35:20 +0100

    Adding dummy router fragment

--- app/src/main/java/com/example/zenoh/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/zenoh/ui/main/SectionsPagerAdapter.java
@@ -21,7 +21,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
-        super(fm);
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         mContext = context;
     }
 
@@ -29,7 +29,11 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        if (position == 0) {
+            return new RouterFragment();
+        } else {
+            return PlaceholderFragment.newInstance(position + 1);
+        }
     }
 
     @Nullable
